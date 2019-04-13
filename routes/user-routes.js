e1da58710714eaf4fac27d604681e6edbde8a538
Sorin Chis <kish.sorin@yahoo.com>
2019-04-14 00:08:52 +0300

    added helpers function for user routes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+//const restricted = require("../middleware/restrictedRoute");  for later
+//const checkRole = require("../middleware/roleCheck");  for later
+
+const userHelpers = require("../database/dbHelpers/userHelpers.js");
 
