676ff41d4f4030e39efc2d29e47570096b9ed8c8
Sorin Chis <kish.sorin@yahoo.com>
2019-04-14 00:10:41 +0300

    added endpoint request for creating a user in database

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -7,3 +7,37 @@ const jwt = require("jsonwebtoken");
 
 const userHelpers = require("../database/dbHelpers/userHelpers.js");
 
+/**
+ *  * [POST] /api/register
+ *
+ * Endpoint for creating/register a new user
+ *
+ * Exemple of payload
+ * {
+ *  name: {string} needs to be unique  - require
+ *  password: {string}   - require
+ *  email: {string}   - require
+ *  role: {string}   - optional for now
+ * }
+ *
+ */
+router.post("/api/register", (req, res) => {
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 12);
+  creds.password = hash;
+  userHelpers
+    .registerUser(creds)
+    .then(user => {
+      //const token = generateToken(user);
+      res.status(200).json({
+        //token,
+        id: user.id,
+        email: user.email,
+        message: `User: ${user.name} was registered succesfully`
+      });
+    })
+    .catch(err => {
+      //console.log(err);
+      res.status(500).json({ err: "eross" });
+    });
+});
