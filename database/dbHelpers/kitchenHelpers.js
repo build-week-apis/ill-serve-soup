9e1ee013c15ffd4557a282030ce170a3d6006cda
Sorin Chis <kish.sorin@yahoo.com>
2019-04-16 14:06:57 +0300

    added database helpers function for kitchen table

--- database/dbHelpers/kitchenHelpers.js
+++ database/dbHelpers/kitchenHelpers.js
@@ -6,6 +6,41 @@ async function getAllSoupKitchen() {
   return allKitchens;
 }
 
+async function getKitchenById(ids) {
+  const kitchen = await db("kitchens")
+    .where({ id })
+    .first();
+
+  return kitchen;
+}
+
+async function addKitchen(kitchen) {
+  const [ids] = await db("kitchens").insert(kitchen);
+  const item = getItemById(ids);
+
+  return item;
+}
+
+async function editKitchen(id, kitchen) {
+  const result = await db("kitchens")
+    .where({ id })
+    .update(kitchen);
+
+  return result;
+}
+
+async function deleteKitchen(id) {
+  const result = await db("kitchens")
+    .where({ id })
+    .del();
+
+  return result;
+}
+
 module.exports = {
-  getAllSoupKitchen
+  getAllSoupKitchen,
+  getKitchenById,
+  addKitchen,
+  editKitchen,
+  deleteKitchen
 };
