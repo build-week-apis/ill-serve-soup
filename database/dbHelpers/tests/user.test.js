2711729836de512d89ab7f6562eb40461a4c37d3
Sorin Chis <kish.sorin@yahoo.com>
2019-04-16 22:27:25 +0300

    added test for adding a user

--- database/dbHelpers/tests/user.test.js
+++ database/dbHelpers/tests/user.test.js
@@ -1,5 +1,32 @@
-const db = require("../userHelpers");
+const dbHelpers = require("../userHelpers");
+const db = require("../../dbConfig");
 
 describe("User endpoints testing", () => {
-  it("add user", () => {});
+  beforeEach(async () => {
+    await db("users").truncate();
+  });
+  afterEach(async () => {
+    await db("users").truncate();
+  });
+
+  it("should set testing enviroment", () => {
+    expect(process.env.DB_ENV).toBe("testing");
+  });
+
+  it("shoud register/add a new user", async () => {
+    await dbHelpers.registerUser({
+      name: "Toby",
+      password: 123,
+      email: "toby@yahoo.com",
+      role: "manager"
+    });
+    await dbHelpers.registerUser({
+      name: "Maria",
+      password: 123,
+      email: "maria@yahoo.com",
+      role: "manager"
+    });
+    const users = await db("users");
+    expect(users).toHaveLength(2);
+  });
 });
