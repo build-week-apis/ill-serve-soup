077f3170f4e7692c6eb663b2d3013e564e6e8cf0
Sorin Chis <kish.sorin@yahoo.com>
2019-04-17 13:34:05 +0300

    refactor code in user tests, added data before all tests, truncate it after all , more DRY code

--- database/dbHelpers/model-tests/user.test.js
+++ database/dbHelpers/model-tests/user.test.js
@@ -2,15 +2,7 @@ const dbHelpers = require("../userHelpers");
 const db = require("../../dbConfig");
 
 describe("User helper function testing", () => {
-  beforeEach(async () => {
-    await db("users").truncate();
-  });
-
-  it("should set testing enviroment", () => {
-    expect(process.env.DB_ENV).toBe("testing");
-  });
-
-  it("shoud register/add a new user", async () => {
+  beforeAll(async () => {
     await dbHelpers.registerUser({
       name: "Toby",
       password: 123,
@@ -23,17 +15,22 @@ describe("User helper function testing", () => {
       email: "maria@yahoo.com",
       role: "manager"
     });
+  });
+
+  afterAll(async () => {
+    await db("users").truncate();
+  });
+
+  it("should set testing enviroment", () => {
+    expect(process.env.DB_ENV).toBe("testing");
+  });
+
+  it("shoud register/add a new user", async () => {
     const users = await db("users");
     expect(users).toHaveLength(2);
   });
 
   it("shoud get a user by id", async () => {
-    await dbHelpers.registerUser({
-      name: "Toby",
-      password: 123,
-      email: "toby@yahoo.com",
-      role: "manager"
-    });
     const user = await dbHelpers.getUserById(1);
     expect(user).toBeDefined();
     expect(user).toEqual({
@@ -45,29 +42,11 @@ describe("User helper function testing", () => {
   });
 
   it("shoud update a user", async () => {
-    await dbHelpers.registerUser({
-      name: "Toby",
-      password: 123,
-      email: "toby@yahoo.com",
-      role: "manager"
-    });
     const result = await dbHelpers.updateUser(1, { name: "Teresa" });
     expect(result).toBe(1);
   });
 
   it("shoud delete a user", async () => {
-    await dbHelpers.registerUser({
-      name: "Toby",
-      password: 123,
-      email: "toby@yahoo.com",
-      role: "manager"
-    });
-    await dbHelpers.registerUser({
-      name: "Maria",
-      password: 123,
-      email: "maria@yahoo.com",
-      role: "manager"
-    });
     const users = await db("users");
     expect(users).toHaveLength(2);
 
